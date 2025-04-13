7a976304f23131f31a14fabc5f70703ff25bdf03
sujit pawar <sujitpawar.scoe.mech@gmail.com>
2025-04-13 12:31:39 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Input from './Components/Input'
+import useCurrency from './hooks/Usecurrency';
+
 
 function App() {
+  
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState('usd');
+  const [toCurrency, setToCurrency] = useState('inr');
+  const currencyData = useCurrency(fromCurrency);
+  const rate = currencyData?.[toCurrency] || 0;
+  const convertedAmount = (amount * rate).toFixed(2);
   return (
 <div className=' w-full h-screen bg-slate-500' >
       <div className='w-1/2 h-1/2 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-3xl bg-slate-300/5 backdrop-blur-[10px] border border-white/30  shadow-2xl p-3' >
       <div className='w-full h-full relative rounded-2xl overflow-hidden '  >
-      <Input />
+      <Input/>
       </div>
       </div>
      <button> <div className='w-1/2 h-1/6 absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-3xl mt-72  bg-slate-300/5 backdrop-blur-[10px] border border-white/30  shadow-2xl p-3 ' >
--- src/hooks/Usecurrency.js
+++ src/hooks/Usecurrency.js
@@ -1,16 +1,17 @@
 import { useState, useEffect } from 'react'
 
-function Usecurrency() {
+function Usecurrency(currencyCode) {
     const [data , setData] = useState({})
     useEffect (()=>{
         if (!currencyCode) return;
-        fetch (`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch (`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currencyCode}.json`)
         .then((res)=> res.json())
         .then((res )=> setData(res[currencyCode]))
+        .catch((err) => console.error(err));    
     },[currencyCode])
     console.log(data);
     return data
 }
 
 export default Usecurrency
-
+    
